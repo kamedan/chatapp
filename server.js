f7cbca391435d282b88eaa639474a42cc6b0de44
DESKTOP-BS34OR0\LENOVO <informatique@blachere-factory.com>
2018-03-17 10:09:30 +0100

    server setup and config

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const http = require('http');
+const container = require('./container');
+
+container.resolve(function(users){
+    const app = setupExpress();
+
+    function setupExpress(){
+        const app = express();
+        const server = http.createServer(app);
+        server.listen(3000, function(){
+            console.log('server on port 3000');
+        });
+
+        ConfigureExpress(app);
+
+        const router = require('express-promise-router')();
+        users.setRouting(router);
+
+         app.use(router);
+
+    }
+
+    
+
+    function ConfigureExpress(app){
+        app.use(express.static('public'));
+        app.set('view engine', 'ejs');
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended: true}));
+    }
+
+
+});
